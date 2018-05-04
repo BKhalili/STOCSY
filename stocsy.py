66537c5e5967557cc4cdfebe3095f6e401d3df4e
Bita Khalili <bita.khalili@unil.ch>
2018-05-04 15:31:14 +0200

    increased efficeincy±

--- stocsy.py
+++ stocsy.py
@@ -6,26 +6,35 @@
 import numpy as np
 
 def correlation(M):
-    T=np.transpose(M)
-    C=np.corrcoef(T)
+    C=np.corrcoef(M,rowvar=0)
     return(C)
 
-def distanceMatrix(n,m):
-    D = np.empty((n,m))
-    for i in range(n):
-        for j in range(m):
-            D[i][j] = abs((i-0.5)/n-(j-0.5)/m)/np.sqrt(1/(n*n)+1/(m*m))
+def distanceMatrix(n):
+    D = np.indices((n,n))
+    D = abs(D[0]-D[1])*1/np.sqrt(2)
     return (D)
 
 def sigCorrMat(M,D,dist,sig):
-    flag_feat = np.zeros(M.shape[0],dtype=int)
-    for j in range(D.shape[1]):
-        for i in range(D.shape[0]):
-            if(D[i][j] > dist):
-                if(M[i][j] > sig):
-                    flag_feat[j] = 1;
-    N=[]
-    for i in range(M.shape[0]):
-        if flag_feat[i]:
-            N.append(M[:][i])
+    flag_feat =np.logical_and(M>sig , D>dist)
+    N = []
+    for j in range(flag_feat.shape[1]):
+            if np.any(flag_feat[:][j]):   #np.any(flag_feat[i]):
+                N.append(M[:][j])
     return (N)
+
+def stocsy(M,dist,sig):
+    print("\n----------Computing correlation matrix------------")
+    M=correlation(M)
+    print('Correlation matrix: '+\
+              '{:d}'.format(M.shape[0])+'x'+'{:d}'.format(M.shape[1]))
+
+    print("\n----------Computing off-diagonal distance------------")
+    D=distanceMatrix(M.shape[0])
+    print('Correlation matrix: '+\
+          '{:d}'.format(D.shape[0])+'x'+'{:d}'.format(D.shape[1]))
+
+    print("\n----------Cunstructing significant correlation  matrix------------")
+    sigCorrM=sigCorrMat(M,D,dist,sig)
+    print('Correlation matrix: '+\
+          '{:d}'.format(len(sigCorrM))+'x'+'{:d}'.format(sigCorrM[0].shape[0]))
+    return(sigCorrM)
--- stocsywrp.py
+++ stocsywrp.py
@@ -13,7 +13,7 @@ def main():
     parser = OptionParser(usage)
     parser.add_option('-i','--inputfile',dest='infile', type='string',default='data.csv')
     parser.add_option('-o','--outputfile',dest='outfile', type='string',default='stocsy.csv')
-    parser.add_option('-d', '--dist',help='off-diagonal distance',dest='dist',type='float',default=3.0)
+    parser.add_option('-d', '--dist',help='off-diagonal distance',dest='dist',type='float',default=0.1)
     parser.add_option('-s', '--significance',help='correlation significance',dest='sig',type='float',default=0.7)
     (options, args) = parser.parse_args()
     print("input file=",options.infile,"\noutput file=",options.outfile,"\noff-diagonal distance=",options.dist,"\ncorrolation significance=",options.sig)
@@ -26,21 +26,8 @@ def main():
     print('data matrix loaded: '+\
               '{:d}'.format(M.shape[0])+'x'+'{:d}'.format(M.shape[1]))
 
-    print("\n----------Computing correlation matrix------------")
-    M=stocsy.correlation(M)
-    print('Correlation matrix: '+\
-      '{:d}'.format(M.shape[0])+'x'+'{:d}'.format(M.shape[1]))
-      
-    print("\n----------Computing off-diagonal distance------------")
-    D=stocsy.distanceMatrix(M.shape[0],M.shape[1])
-    print('Correlation matrix: '+\
-          '{:d}'.format(D.shape[0])+'x'+'{:d}'.format(D.shape[1]))
-    
-    print("\n----------Cunstructing significant correlation  matrix------------")
-    sigCorrMat=stocsy.sigCorrMat(M,D,options.dist,options.sig)
-    print('Correlation matrix: '+\
-          '{:d}'.format(len(sigCorrMat))+'x'+'{:d}'.format(sigCorrMat[0].shape[0]))
-          
+    sigCorrMat=stocsy.stocsy(M,options.dist,options.sig)
+
     print("\n----------wrting output file------------")
     output=pd.DataFrame(sigCorrMat)
     output=output.transpose()
