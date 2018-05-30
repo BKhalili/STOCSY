a8ee4f26a3b2ac2d4e9b308fd32c2f427909f2ea
Bita <bita@wl6s-137-91.unil.ch>
2018-05-30 10:32:11 +0200

    stocsy producing pseudospectrums by averaging the feature-feature correlations of every two highly correlated features.

--- stocsy.py
+++ stocsy.py
@@ -4,37 +4,78 @@
 #*************************
 
 import numpy as np
+import pandas as pd
+from scipy import stats
+import sys
+import matplotlib.pyplot as plt
+from copy import deepcopy
 
-def correlation(M):
-    C=np.corrcoef(M,rowvar=0)
-    return(C)
 
-def distanceMatrix(n):
-    D = np.indices((n,n))
-    D = abs(D[0]-D[1])*1/np.sqrt(2)
-    return (D)
-
-def sigCorrMat(M,D,dist,sig):
-    flag_feat =np.logical_and(M>sig , D>dist)
-    N = []
-    for j in range(flag_feat.shape[1]):
-            if np.any(flag_feat[:][j]):   #np.any(flag_feat[i]):
-                N.append(M[:][j])
-    return (N)
-
-def stocsy(M,dist,sig):
+def stocsy(M,ppm,dist,sig):
     print("\n----------Computing correlation matrix------------")
     M=correlation(M)
     print('Correlation matrix: '+\
-              '{:d}'.format(M.shape[0])+'x'+'{:d}'.format(M.shape[1]))
-
+          '{:d}'.format(M.shape[0])+'x'+'{:d}'.format(M.shape[1]))
+        
     print("\n----------Computing off-diagonal distance------------")
-    D=distanceMatrix(M.shape[0])
+    D=distanceMatrix(M.shape[0],ppm)
     print('Correlation matrix: '+\
-          '{:d}'.format(D.shape[0])+'x'+'{:d}'.format(D.shape[1]))
-
+        '{:d}'.format(D.shape[0])+'x'+'{:d}'.format(D.shape[1]))
+          
     print("\n----------Cunstructing significant correlation  matrix------------")
-    sigCorrM=sigCorrMat(M,D,dist,sig)
-    print('Correlation matrix: '+\
-          '{:d}'.format(len(sigCorrM))+'x'+'{:d}'.format(sigCorrM[0].shape[0]))
-    return(sigCorrM)
+    M=M.values
+    #output=pd.DataFrame(M)
+    #output.to_csv('CorrMatrix.csv',index=False,header=None)
+    #print(M[0])
+    #print('in stocsy.stocsy before maxCorrPairs')
+    P=maxCorrPairs(deepcopy(M),D,dist,sig,ppm)
+    #print(M[0])
+    #print('in stocsy.stocsy after maxCorrPairs')
+    print('Number of feature pairs with significant correlation : '+\
+        '{:d}'.format(P.shape[0]))
+    pseudospec=pseudospectrum(M,P)   #a list of pseusospecs each correspond to the order of pairs in P
+    return(pseudospec)
+
+def correlation(M):
+    C=M.corr(method='pearson',min_periods=1)
+    return(C)
+
+def distanceMatrix(n,ppm):
+    D = np.matmul(np.diag(ppm),np.ones((n,n)))   #a matrix with repeatition of ppms as columns
+    D = abs(D-np.transpose(D))
+    return (D)
+
+def maxCorrPairs(CorrMat,DistMat,dist,sig,ppm):
+    P = []                  #list of pairs with max corr
+    for i in range(CorrMat.shape[0]):
+        flag_feat =np.logical_and(CorrMat[i]>sig , DistMat[i]>dist)
+        if np.any(flag_feat):   #if any of the off diagonal corrolation elements was significant
+            featCorr=CorrMat[i]
+            featCorr[np.invert(flag_feat)]=0     #to find the argmax among significant off-diagonal elements put to zero all the insignificant and diagonal elements of the featCorr
+            P.append(np.array([i,np.argmax(featCorr)]))
+
+            #print(np.array([i,np.argmax(featCorr)]))
+    removing_similar=np.ones(len(P))
+    for i in range(len(P)):
+        for j in range(i+1,len(P)):
+            if P[i][0]==P[j][1] and P[i][1]==P[j][0]:
+                removing_similar[j]=0
+
+    removing_similar=removing_similar>0
+    #print(removing_similar)
+    maxCorrPairMat=np.array(P)
+    #print(maxCorrPairMat[:])
+    #print(len(maxCorrPairMat))
+    maxCorrPairMat=maxCorrPairMat[removing_similar]
+    #print(maxCorrPairMat[:])
+    #print(len(maxCorrPairMat))
+    return (maxCorrPairMat)
+
+def pseudospectrum(M,P):
+    pseudospec=[]
+    #print('*****in pseudospectrum*****')
+    for i in range(len(P)):
+        v=0.5*(M[P[i][0]]+M[P[i][1]])
+        #print(v)
+        pseudospec.append(v)
+    return(pseudospec)
--- stocsywrp.py
+++ stocsywrp.py
@@ -6,6 +6,7 @@
 import stocsy
 import pandas as pd
 from optparse import OptionParser
+import sys
 
 def main():
     print("----------Asigning parameters------------")
@@ -14,24 +15,26 @@ def main():
     parser.add_option('-i','--inputfile',dest='infile', type='string',default='data.csv')
     parser.add_option('-o','--outputfile',dest='outfile', type='string',default='stocsy.csv')
     parser.add_option('-d', '--dist',help='off-diagonal distance',dest='dist',type='float',default=0.1)
-    parser.add_option('-s', '--significance',help='correlation significance',dest='sig',type='float',default=0.7)
+    parser.add_option('-s', '--significance',help='correlation significance',dest='sig',type='float',default=0.8)
     (options, args) = parser.parse_args()
     print("input file=",options.infile,"\noutput file=",options.outfile,"\noff-diagonal distance=",options.dist,"\ncorrolation significance=",options.sig)
 
     print("\n----------Reading input file------------")
-    M = pd.read_csv(options.infile,header=0,index_col=0)
-    M = M.fillna(0)       #filling any missing element by 0
-    M = M.values
-    
+    D = pd.read_csv(options.infile,header=None)
+    M = D.loc[1:,1:]
+    ppm = D.loc[0,:]
+    ppm=ppm.drop(labels=0)
+    ppm=ppm.values
+    #fillna(0) filling any missing element by 0
+    #M = M.values
     print('data matrix loaded: '+\
               '{:d}'.format(M.shape[0])+'x'+'{:d}'.format(M.shape[1]))
 
-    sigCorrMat=stocsy.stocsy(M,options.dist,options.sig)
+    pseudospectrums=stocsy.stocsy(M,ppm,options.dist,options.sig)
 
-    print("\n----------wrting output file------------")
-    output=pd.DataFrame(sigCorrMat)
-    output=output.transpose()
-    output.to_csv(options.outfile)
+    print("\n----------writing output file------------")
+    output=pd.DataFrame(pseudospectrums)
+    output.to_csv(options.outfile,index=False,header=None)
 
 if __name__ == '__main__':
     main()
